4a2d01a3e871490aeb2ca83fdc2ed144c968516d
SardanaMir <sardanadord@yandex.ru>
2023-10-24 17:43:40 +0300

    added research feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const URL = "https://api.giphy.com/v1/gifs/search?api_key=";
     const APIKEY = "YIsjFowb5cokIEmiQ9t4LaIkjZt43SCn";
+
     function init(){
         event.preventDefault();
         let input = document.getElementById('search').value.trim();
@@ -10,14 +11,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
             .catch(error => console.log(error))
         })
 
+        let gifClasses = document.querySelectorAll('.gif');
+        console.log(gifClasses)
+        for (let i = 0; i < gifClasses.length; i++){
+            gifClasses[i].remove()
+        }
+
         Promise.all([promise_1]).then(data => {
             let gifsCollection = data[0].data;
             let out = document.querySelector('.out')
             for (let i = 0; i < gifsCollection.length; i++){
                 let gifUrl = data[0].data[i].images.downsized.url;
-                let newGif = document.createElement('img') ;
+                let newGif = document.createElement('img');
                 newGif.src = gifUrl; 
-                newGif.alt = data[0].data[i].title
+                newGif.alt = data[0].data[i].title;
+                newGif.classList.add('gif')
                 out.append(newGif)
             }
         })

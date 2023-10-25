1bab2f00aada39fd847c70e0973745b8ff8f4f0f
SardanaMir <sardanadord@yandex.ru>
2023-10-25 13:07:19 +0300

    add file structure and adaptive

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const URL = "https://api.giphy.com/v1/gifs/search?api_key=";
-    const APIKEY = "YIsjFowb5cokIEmiQ9t4LaIkjZt43SCn";
 
     function init(){
         event.preventDefault();
-        let input = document.getElementById('search').value.trim();
+        let input = document.getElementById('search');
+        //отправка запроса
         const promise_1 = new Promise((resolve) =>{
-            fetch(`${URL}${APIKEY}&q=${input}&limit=25`)
+            fetch(`${URL}${APIKEY}&q=${input.value.trim()}&limit=25`)
             .then(data => resolve(data.json()))
             .catch(error => console.log(error))
         })
-
+        //удаление существующих гифок со страницы
         let gifClasses = document.querySelectorAll('.gif');
         console.log(gifClasses)
         for (let i = 0; i < gifClasses.length; i++){
             gifClasses[i].remove()
         }
-
+        //очищение поиска
+        input.value = '';
+        // вывод гифок на страницу
         Promise.all([promise_1]).then(data => {
             let gifsCollection = data[0].data;
             let out = document.querySelector('.out')
